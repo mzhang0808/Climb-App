42c44d6774aa5c830ebc319d5d9f379e7822abea
Johnson Ku <johnsonku@cs.ucsb.edu>
2020-01-23 18:38:35 -0800

    jk/cs create bye button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 function greet() {
   alert('Ganesh and Michael say Hi!');
 }
-
+function bye() {
+  alert('Johnson and Cody say Bye!');
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={greet}>
           Say Hi to Ganesh and Michael
+        </button>
+        <button onClick={bye}>
+          Say Bye to Johnson and Cody
         </button>
       </header>
     </div>
