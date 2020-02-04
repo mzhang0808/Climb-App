8f58b1a28d1c19d4e7ccd9124fdb2a62e3d9346d
Timothy Yuan <timothyuan@gmail.com>
2020-02-03 17:17:19 -0800

    api endpoint to get all users from heroku postgres users table

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,32 @@
 var cors = require('cors');
 var express = require('express');
+
 var app = express();
+
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'qjyqteuktgdkur',
+  host: 'ec2-23-21-13-88.compute-1.amazonaws.com',
+  database: 'd995g1c339k78d',
+  password: '02f4dcfe5c71902626923dfce5086de3c814000204268df9ac04eece3d8100ae',
+  port: 5432,
+  ssl: true
+})
 
 app.use(cors());
 
 app.get('/hello', function(req, res) {
   res.send("hello world");
 });
+
+app.get('/users', function(req, res) {
+  pool.query('SELECT * FROM users;', (err, res) => {
+    if (err) throw err;
+    console.log(JSON.stringify(res.rows));
+  });
+});
+
+
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
