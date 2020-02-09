baa8f4ea1883db967fcd8ab55d9cfd619054aaeb
Albert Li <albertli@ResNet-35-172.resnet.ucsb.edu>
2020-02-08 19:27:19 -0800

    Added endpoints for competitions and scores

--- backend/app.js
+++ backend/app.js
@@ -22,8 +22,17 @@ app.get('/hello', function(req, res) {
   res.send("hello world");
 });
 
+
+// Users endpoint
 app.get('/users', function(req, res) {
   pool.query("SELECT * FROM users;", (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });
+});
+
+app.get('/users/:name', function(req, res) {
+  pool.query("SELECT * FROM users where user_name='"+req.params.name+"';", (err, response) => {
     if (err) throw err;
     res.send(response.rows);
   });
@@ -39,6 +48,61 @@ app.post('/users', function(req, res) {
     res.send(response);
   });
 });
+
+// Competitions endpoint
+app.get('/competitions', function(req, res) {
+  pool.query("SELECT * FROM competitions;", (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });
+});
+
+app.post('/competitions', function(req, res) {
+  var comp = req.body.comp_name;
+  var numProb = req.body.num_of_problems;
+  pool.query("INSERT INTO competitions (comp_name, num_of_problems) VALUES ('"+comp+"', '"+numProb+"');", (err, response) => {
+    if (err){
+      res.send(err);
+    }
+    res.send(response);
+  });
+});
+
+// Scores endpoint
+app.get('/scores', function(req, res) {
+  pool.query("SELECT * FROM scores;", (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });
+});
+
+app.post('/scores', function(req, res) {
+  var user = req.body.user_name;
+  var comp = req.body.comp;
+  pool.query("UPDATE users SET current_comp = '"+ comp +"' where user_name = '"+ user +"';");
+  pool.query("INSERT INTO scores (user_name, comp, problems) VALUES ('"+user+"', '"+comp+"', ARRAY[]::completed[]);", (err, response) => {
+    if (err){
+      res.send(err);
+    }
+    res.send(response);
+  });
+});
+
+app.patch('/scores/:name/:comp', function(req, res) {
+  var problem = req.body.problem;
+  var attempts = req.body.attempts;
+
+  pool.query("UPDATE scores SET problems[CARDINALITY(problems)+1] = ROW("+ problem +","+ attempts +") where user_name= '"+ req.params.name + "' and comp= '"+ req.params.comp +"';", (err, response) => {
+    
+    if (err){
+      res.send(err);
+    }
+    res.send(response);
+  });
+});
+
+
+
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
