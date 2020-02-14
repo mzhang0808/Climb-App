d85e28b84d48e5707360ff6747bca791eef82e5e
Timothy Yuan <timothyuan@gmail.com>
2020-02-13 18:26:34 -0800

    add config.js

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    user : 'xqdnhsvcdmopvx',
+    host : 'ec2-54-83-55-122.compute-1.amazonaws.com',
+    database : 'd1upulvgjib8qo',
+    password : '864e5bcd2441c503f96b5b7fc1fb218271d5a1accd2e430ebe85126cf8249f02',
+    port : 5432,
+    ssl : true
+}
